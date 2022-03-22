77a1ccb0510519907ca11e4c7630c1ecad4b7c38
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2022-03-22 16:33:50 -0300

    Se borra archivos en desuso

--- DESAFIO-5-CLASE-10/DESAFIO/TEMPLATE_HBS/index.js
+++
@@ -1,8 +0,0 @@
-const COLORS = require('colors');
-const APP = require('./app');
-
-const PORT = 8080;
-
-const _server = APP.listen(PORT, () => console.log(`SERVIDOR CORRIENDO EN EL PUERTO ${_server.address().port}`.green));
-
-_server.on('error', error => console.log(`ERROR EN EL SERVIDOR: ${error}`.red));
