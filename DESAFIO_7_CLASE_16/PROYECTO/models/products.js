ea07c7b1f869cca146f2e1eb3af32ed31142cf34
Facundo Gonzales Liendo <facundogonzalesliendo@gmail.com>
2022-04-13 00:11:55 -0300

    Se arman metodos en modelo para base

--- DESAFIO_7_CLASE_16/PROYECTO/models/products.js
+++ DESAFIO_7_CLASE_16/PROYECTO/models/products.js
@@ -1,4 +1,5 @@
 const FS = require('fs');
+const knex = require("../dataBase/mysql");
 
 class Products {
     
@@ -9,24 +10,17 @@ class Products {
     // Devuelve un array con los objetos presentes en el archivo.
     async get(id) {
         try{
-            // buscamos el archivo de productos.
-            let response = await FS.promises.readFile(`./${this.file}.txt`,'utf-8');
+            let resp;
 
-            // lo almacenamos en un listado parseandolo
-            let _list = JSON.parse(response);
-            console.log('jppaaa')
- 
-            // Si no recibimos un id, retornamos el listado completo.
-            if(!id) return _list;
+            if(!id) return resp = await knex.select("*").from("productos");
+            
+            resp = await knex.select("*").from("productos").where({ id });
 
-            // Si tenemos un id, vamos a buscar dentro del archivo.    
-            let prodSelected = _list.find(element => element.id === id);
+            if (resp.length > 0) return resp[0];
+              
+            return (`No existe el producto con id: ${id}`);
+              
 
-            // Si no encontramos elemento, retornamos error.
-            if(!prodSelected) throw new Error();
-                
-            // Si el producto existe, lo retornamos.
-            return prodSelected;
         }catch(err){
             return {
                 status: 'Error', 
@@ -39,10 +33,6 @@ class Products {
     // recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
     async save( element ){
         try {
-            let response = await FS.promises.readFile(`./${this.file}.txt`,'utf-8');
-
-            let _list = JSON.parse(response);
-
             const newProduct = {
                 id: _list[_list.length-1].id + 1,
                 timestamp: element.timestamp,
@@ -54,22 +44,23 @@ class Products {
                 stock: element.stock
             }
 
-            _list.push(newProduct); 
+            const resp = await knex("productos").insert(newProduct);
 
-            try {
-                await FS.promises.writeFile(`./${this.file}.txt`, JSON.stringify(_list,null,2));
-                return newProduct.id 
-            }catch(err) {
-                return {
-                    status: 'Error', 
-                    message: 'Error al cargar el producto.', 
-                    error: err
-                }
-            }
+            return resp;
 
         }catch(err) {
-            const createProduct = {
-                id: 1,
+            return {
+                status: 'Error', 
+                message: 'Error al crear el archivo y producto.', 
+                error: err
+            }
+            
+        }
+    }
+
+    async update(id, element){
+        try{
+            const productUpdate = {
                 timestamp: element.timestamp,
                 name: element.name,
                 description: element.description,
@@ -78,64 +69,9 @@ class Products {
                 code: element.code,
                 stock: element.stock
             }
-
-            try {
-                await FS.promises.writeFile(`./${this.file}.txt`,JSON.stringify([createProduct],null,2));
-                return createProduct.id;
-            } catch (err) {
-                return {
-                    status: 'Error', 
-                    message: 'Error al crear el archivo y producto.', 
-                    error: err
-                }
-            }
-        }
-    }
-
-    async update(id, object){
-        try{
-            let response = await FS.promises.readFile(`./${this.file}.txt`,'utf-8');
-            let _list = JSON.parse(response);
-
-            if (!_list.find(element => element.id == id)) throw new Error();
- 
-            for (let i = 0; i < _list.length; i++) {
-                if(_list[i].id == id){
-                  _list[i].timestamp = producto.timestamp,
-                  _list[i].name = producto.name,
-                  _list[i].description = producto.description,
-                  _list[i].price = parseInt(producto.price),
-                  _list[i].thumbnail = producto.thumbnail,
-                  _list[i].code = producto.code,
-                  _list[i].stock = producto.stock
-                };
-
-
-            };
-
-            save(_list).then( res.send("Producto actualizado"))
-    
-            this.index()
-            .then(async el=>{
-                let aux = el
-                const index = aux.findIndex(el=>el.id == id)
-
-                aux[index].timestamp = object.timestamp || "",
-                aux[index].name = object.name || "",
-                aux[index].description = object.description || "",
-                aux[index].price = parseInt(object.price) || 0,
-                aux[index].thumbnail = object.thumbnail || "",
-                aux[index].code = object.code || "",
-                aux[index].stock = object.stock || ""
-
-                await FS.promises.writeFile(url, JSON.stringify(aux) )
-            
-            })
-            .catch(error=>{
-                console.log("No se pudo eliminar el objeto.",error)
-            })
-
-
+              
+            const resp = await knex("productos").where({ id: id }).update(productUpdate);
+            return resp;
 
         }catch(err){
             return {
@@ -149,26 +85,8 @@ class Products {
     // Elimina del archivo el objeto con el id buscado.
     async deleteById(id) {
         try{
-            let response = await FS.promises.readFile(`./${this.archivo}.txt`,'utf-8');
-    
-            let _list = JSON.parse(response);
-    
-            if(!_list.find(element=>element.id === id)) throw new Error();
-            
-            let _updateProd = _list.filter(el=>el.id !== id);
-    
-            try {
-                await FS.promises.writeFile(`./${this.file}.txt`,JSON.stringify(_updateProd,null,2))
-                return {
-                    status: 'Success', 
-                    message: 'Producto eliminado con éxito.'
-                }
-            }catch(err){
-                return {
-                    status: 'Error', 
-                    message: 'Hubo un problema al borrar el producto.'
-                }
-            } 
+            const resp = await knex("productos").where({ id }).del();
+            return resp;
         }catch(err){
             return {
                 status: 'Error', 
